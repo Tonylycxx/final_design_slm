a2602d4cbdd068f865e5d85bd9983b360ebdfda1
Tonylyc <2290549567@qq.com>
2025-05-22 19:27:38 +0800

    test

--- python/tvm/auto_scheduler/cost_model/tlp_model.py
+++ python/tvm/auto_scheduler/cost_model/tlp_model.py
@@ -37,6 +37,6 @@ class SlmPModel(PythonBasedModel):
     def load(self, file_name: str):
         with open(file_name, 'rb') as f:
             device = "cuda:0" if torch.cuda.is_available() else "cpu"
-            self.model = pickle.load(f, map_location=device).module.to(device)
+            self.model = torch.load(f, map_location=device).module.to(device)
 
         self.model.eval()
